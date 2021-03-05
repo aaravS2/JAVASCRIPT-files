2291736342134810c641ff663c4954cfad7921aa
aaravS2 <71128251+aaravS2@users.noreply.github.com>
2021-03-05 16:36:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+//all the variables
+function preload(){
+//put preload code here
+}
+function setup() {
+// put setup code here
+createCanvas(800,1230);
+}
+// put drawing code here
+function draw() {
+background(0)
+drawSprites();
+}
